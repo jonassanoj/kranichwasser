12c7049f3b322bb24eff122b5432f3c698829e29
jonas <jonas.bucher@campus.tu-berlin.de>
2017-03-01 20:31:48 +0100

    works with all 8 GPIOs with URL call, valveId, RainTime

--- raspi/led.py
+++ raspi/led.py
@@ -1,9 +1,14 @@
-import urllib
+import urllib2
 import time
 
-fun open():
-    return urllib.urlopen('http://192.168.0.110/?pin=ON1')
-time.sleep(5)
-fun open():
-    return urllib.urlopen('http://192.168.0.110/?pin=OFF1')
-time.sleep(5)
+def rain(controllerURL, valveID, rainTime):
+    for x in range(1, 9):
+        if (x==valveID):
+            urllib2.urlopen(controllerURL + "/?pin=ON" + str(x)).read()
+            time.sleep(rainTime)
+            urllib2.urlopen(controllerURL + "/?pin=OFF" + str(x)).read()
+            time.sleep(1)
+            print(x)
+
+# function call: controllerURL, valveID, rainTime
+rain ("http://192.168.0.110", 1, 1)
