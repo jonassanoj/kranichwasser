d66a9933eb54d1b5a312d177a11218f6fa163e50
herrgaertner <philipp.gaertner@gmail.com>
2017-03-01 17:34:40 +0100

    Add files via upload

---
+++ led.py
@@ -0,0 +1,9 @@
+import urllib
+import time
+
+fun open():
+    return urllib.urlopen('http://192.168.0.110/?pin=ON1')
+time.sleep(5)
+fun open():
+    return urllib.urlopen('http://192.168.0.110/?pin=OFF1')
+time.sleep(5)
